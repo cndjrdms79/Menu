ff9c8ecb9bba31dddb5b749406b8d2c6b546b39e
20191126@student.anu.ac.kr <20191126@student.anu.ac.kr>
2019-10-20 19:31:59 +0900

    초기 커밋.

---
+++ MenuDlg.h
@@ -0,0 +1,39 @@
+﻿
+// MenuDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMenuDlg 대화 상자
+class CMenuDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMenuDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MENU_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnContextMenu(CWnd* /*pWnd*/, CPoint /*point*/);
+	afx_msg void OnMenuFont();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+};
